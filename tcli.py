9a8ffbc3d13a1f114160b9871dfe3410927d3c40
inassatus <inassatus@gmail.com>
2021-01-19 15:29:32 +0900

    Update tcli.py
    working version, but does not support those client via data tethering. No relay server call implemented.
    Must add: ping in every 150sec after the initial ping.

--- tcli.py
+++ tcli.py
@@ -1,6 +1,8 @@
 import socket
 from threading import Thread
 import time
+import select
+
 
 def b2addr(comb):
 	comb = comb.decode()
@@ -12,25 +14,31 @@ def b2addr(comb):
 
 def pingudp(socket, addr):
 	for i in range(50):
-		print("sending ping to ", addr)
+		socket.sendto("ping".encode(), addr)
+		time.sleep(150)
+
+def initialping(socket, addr):
+	for i in range(50):
 		socket.sendto("ping".encode(), addr)
 		time.sleep(1)
 
+def get_initialping(socket):
+	pass
 
 
 ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ip = "1.248.169.25"
+ip = "192.168.0.10"
 port = 80
 saddr = (ip, port)
 
-
+print("request to ", (ip, port))
 ss.sendto(b'0', (ip, port))
 data, addr = ss.recvfrom(128)
 addr = b2addr(data)
 print(addr)
 
 
-sending = Thread(target=pingudp, args=(ss, addr))
+sending = Thread(target=initialping, args=(ss, addr))
 sending.start()
 data, addr = ss.recvfrom(128)
 print(data, addr)
